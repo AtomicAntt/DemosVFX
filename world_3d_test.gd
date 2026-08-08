18de264a48805f5fe9d118b935be0f7d7057ad04
AtomicAntt <anthonychi36@gmail.com>
2026-08-07 21:06:32 -0700

    Experiment more with wireframe -> normal world effect shaders

--- world_3d_test.gd
+++ world_3d_test.gd
@@ -1,29 +1,47 @@
 @tool
 extends Node3D
 
-## Direct setting to the effect radius global shader parameter.
-#@export var effect_radius: float = 0.0:
-	#set(new_radius):
-		#effect_radius = new_radius
-		#set_effect_radius(new_radius)
-		
 @export_tool_button("Animate Effect FadeIn", "Callable") var animate_action = animate
 @export_tool_button("Animate Effect FadeOut", "Callable") var animation_action_out = animate_out
-
 @export_tool_button("Reset", "Callable") var reset_action = reset
 
+var tween: Tween
+
+## In m/s
+@export var world_effect_speed: float = 20.0
+## In m
+@export var world_effect_radius: float = 50.0
+
+@export var world_effect_origin: Marker3D
+
 func animate() -> void:
-	var tween: Tween = create_tween()
-	tween.tween_method(set_effect_radius, 0.0, 50.0, 5.0)
-	await tween.finished
+	set_effect_origin()
+	reset()
+	tween = create_tween()
+	tween.tween_method(set_wireframe_effect_radius, 0.0, world_effect_radius, world_effect_radius / world_effect_speed)
+	tween.tween_method(set_effect_radius, 0.0, world_effect_radius, world_effect_radius / world_effect_speed)
 
 func animate_out() -> void:
-	var tween: Tween = create_tween()
-	tween.tween_method(set_effect_radius, RenderingServer.global_shader_parameter_get("effect_radius"), 0.0, RenderingServer.global_shader_parameter_get("effect_radius")/10.0)
-	await tween.finished
+	set_effect_origin()
+	set_effect_radius(world_effect_radius)
+	set_wireframe_effect_radius(world_effect_radius)
+	
+	tween = create_tween()
+	tween.tween_method(set_effect_radius, world_effect_radius, 0.0, world_effect_radius / world_effect_speed)
+	tween.tween_method(set_wireframe_effect_radius, world_effect_radius, 0.0, world_effect_radius / world_effect_speed)
 
 func reset() -> void:
+	if is_instance_valid(tween):
+		tween.stop()
 	set_effect_radius(0.0)
+	set_wireframe_effect_radius(0.0)
 
 func set_effect_radius(new_radius: float) -> void:
 	RenderingServer.global_shader_parameter_set("effect_radius", new_radius)
+
+func set_wireframe_effect_radius(new_radius: float) -> void:
+	RenderingServer.global_shader_parameter_set("wireframe_effect_radius", new_radius)
+
+func set_effect_origin() -> void:
+	if is_instance_valid(world_effect_origin):
+		RenderingServer.global_shader_parameter_set("effect_origin", world_effect_origin.global_position)
