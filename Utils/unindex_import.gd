fa973a6434e331076593b5ff19d0422e094f92d6
AtomicAntt <anthonychi36@gmail.com>
2026-08-11 21:06:04 -0700

    Add a custom import script that deindexes the model

--- Utils/unindex_import.gd
+++ Utils/unindex_import.gd
@@ -1,15 +1,24 @@
-@tool # Needed so it runs in editor.
+@tool
 extends EditorScenePostImport
 
-# This sample changes all node names.
-# Called right after the scene is imported and gets the root node.
-func _post_import(scene):
-	# Change all node names to "modified_[oldnodename]"
-	iterate(scene)
-	return scene # Remember to return the imported scene
+func _post_import(scene: Node) -> Node:
+	_process_node(scene)
+	return scene
 
-func iterate(node):
-	if node != null and node is MeshInstance3D:
-		node.name = "modified_" + node.name
-		for child in node.get_children():
-			iterate(child)
+func _process_node(node: Node):
+	if node is MeshInstance3D and node.mesh != null:
+		var st = SurfaceTool.new()
+		var new_mesh = ArrayMesh.new()
+		
+		# Rebuild every surface (material slot) of the mesh
+		for i in range(node.mesh.get_surface_count()):
+			st.create_from(node.mesh, i)
+			st.deindex()
+			st.commit(new_mesh)
+			
+		# Overwrite the imported mesh with our de-indexed version
+		node.mesh = new_mesh
+		
+	# Recursively check all child nodes in the imported scene
+	for child in node.get_children():
+		_process_node(child)
